5f765663fff4bafbbbcb407f602faff9b73335a8
Travis Chen <travis.xin@gmail.com>
2019-12-03 17:11:25 +0800

    烤山藥

--- fsm.py
+++ fsm.py
@@ -1,33 +1,6 @@
 from transitions.extensions import GraphMachine
 
-from utils import send_text_messagefrom linebot import LineBotApi
-from linebot.models import TextSendMessage, ImageSendMessage
-from linebot.exceptions import LineBotApiError
-
-CHANNEL_ACCESS_TOKEN = "YOUR CHANNEL TOKEN"
-to = "YOUR USER ID"
-
-line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
-
-#文字訊息
-
-try:
-    line_bot_api.push_message(to, TextSendMessage(text='台科大電腦研習社'))
-except LineBotApiError as e:
-    # error handle
-    raise e
-
-#圖片訊息
-# ImageSendMessage物件中的輸入
-# original_content_url 以及 preview_image_url都要寫才不會報錯。
-#輸入的網址要是一個圖片，應該說只能是一個圖片，不然不會報錯但是傳過去是灰色不能用的圖
-line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
-image_url = "https://i.imgur.com/eTldj2E.png?1"
-try:
-    line_bot_api.push_message(to, ImageSendMessage(original_content_url=image_url, preview_image_url=image_url))
-except LineBotApiError as e:
-    # error handle
-    raise e
+from utils import send_text_message
 
 
 class TocMachine(GraphMachine):
@@ -46,7 +19,7 @@ class TocMachine(GraphMachine):
         print("I'm entering state1")
 
         reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state1")
+        send_text_message(reply_token, "謝謝媽媽 媽媽真好")
         self.go_back()
 
     def on_exit_state1(self):
@@ -56,7 +29,7 @@ class TocMachine(GraphMachine):
         print("I'm entering state2")
 
         reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state2")
+        send_text_message(reply_token, "嬤嬤我想吃烤山藥")
         self.go_back()
 
     def on_exit_state2(self):
