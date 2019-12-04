29c6a4c07effb26d34eb759b26eb69c0ea9a69a6
Travis Chen <travis.xin@gmail.com>
2019-12-04 15:58:26 +0800

    uygukyfggyllg

--- app.py
+++ app.py
@@ -227,7 +227,7 @@ def webhook_handler():
         print(f"REQUEST BODY: \n{body}")
         response = machine.advance(event)
         if response == False:
-            send_text_message(event.reply_token, "Not Entering any State")
+            line_bot_api.reply_message(event.reply_token, "Not Entering any State")
 
     return "OK"
 
