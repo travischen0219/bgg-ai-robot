5c53b7b2d86ceed63f5d932d13f3467e2e8ad79d
Travis Chen <travis.xin@gmail.com>
2019-12-04 15:11:33 +0800

    akerhgukaegruhqelargkuqulaegluaerg

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import os
 
 from linebot import LineBotApi, WebhookParser
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage ,TemplateSendMessage
 
 
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
