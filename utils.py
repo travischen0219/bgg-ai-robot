05c6b69a308d0361434058c17f995a7d0103998b
Travis Chen <travis.xin@gmail.com>
2019-12-08 04:11:18 +0800

    643663584

--- utils.py
+++ utils.py
@@ -12,23 +12,21 @@ def send_text_message(reply_token, title, text, imgurl, option1, option2):
     line_bot_api = LineBotApi(channel_access_token)
     
     buttons_template = TemplateSendMessage(
-        alt_text='請到聊天視窗中確認訊息',
+        alt_text="請到聊天視窗中確認訊息！",
         template=ButtonsTemplate(
             title = title,
             text = text,
             thumbnail_image_url = imgurl,
 
             actions=[
-                if expression:option1
-                    MessageTemplateAction(
-                        label=option1,
-                        text=option1
-                    ),
-                if expression:option2
-                    MessageTemplateAction(
-                        label=option2,
-                        text=option2
-                    ),
+                MessageTemplateAction(
+                    label=option1,
+                    text=option1
+                ),
+                MessageTemplateAction(
+                    label=option2,
+                    text=option2
+                ),
                 URITemplateAction(
                     label='立即觀看「玫瑰瞳鈴眼第51集」',
                     uri='https://www.youtube.com/watch?v=alD7ixZQKso'
